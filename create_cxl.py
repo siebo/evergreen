b4ca14fcc0d00ce985ea5164d96f0f9aa367587c
David Siedband <david@zentraal.com>
2017-04-15 01:25:22 -0700

    streamline imports

--- create_cxl.py
+++ create_cxl.py
@@ -1,6 +1,6 @@
 import csv
 import sys
-import xml.etree.ElementTree
+from xml.etree import ElementTree
 
 # Load CSV data
 f = open(sys.argv[1], 'rt')
@@ -24,14 +24,14 @@ finally:
     f.close()
 
 # Open the blank CXL template
-e = xml.etree.ElementTree.parse('template.cxl')
+e = ElementTree.parse('template.cxl')
 root = e.getroot()
 
 # add concept nodes to the elementtree
 for child in root:
   if child.tag == '{http://cmap.ihmc.us/xml/cmap/}map':
     for concept in concepts:
-      new = xml.etree.ElementTree.Element("concept", label=concept)
+      new = ElementTree.Element("concept", label=concept)
       child.append(new)
 
 e.write('output.cxl')
