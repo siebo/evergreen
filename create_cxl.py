c97465ab34262df651d62c58df2bd26373b18d11
David Siedband <david@zentraal.com>
2017-04-15 02:44:41 -0700

    generating unique IDs

--- create_cxl.py
+++ create_cxl.py
@@ -1,11 +1,28 @@
 import csv
 import sys
 from xml.etree import ElementTree
+from string import ascii_uppercase
+from string import digits
+from random import SystemRandom
+
 
 ElementTree.register_namespace('', "http://cmap.ihmc.us/xml/cmap/")
 ElementTree.register_namespace('dcterms', "http://purl.org/dc/terms/")
 ElementTree.register_namespace('dc', "http://purl.org/dc/elements/1.1/")
 ElementTree.register_namespace('vcard', "http://www.w3.org/2001/vcard-rdf/3.0#")
+
+
+def createCmapID():
+  options = ascii_uppercase + digits
+  i1 = ''.join(SystemRandom().choice(options) for _ in range(9))
+  i2 = ''.join(SystemRandom().choice(options) for _ in range(6))
+  i3 = ''.join(SystemRandom().choice(options) for _ in range(2))
+  new_id = "%s-%s-%s" % (i1,i2,i3)
+  return new_id
+
+
+print createCmapID()
+
 
 # Load CSV data
 f = open(sys.argv[1], 'rt')
