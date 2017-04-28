d0a1b02b3e64227c63a224ace27d71c4c2b689a5
mateolan <mateolan@gmail.com>
2017-04-28 00:10:25 -0700

    Fixed cmapPostMap.py to use Test Concept Map3.cxl. so that saving cmaps with nodes works now. Comments should be read carefully

--- cmapPostMap.py
+++ cmapPostMap.py
@@ -9,7 +9,11 @@ baseURL = 'https://cmapscloud.ihmc.us/resources/rid='
 mime_type = 'application/xml'
 
 # pass folder ID on command line
-# for example 1QBMKWB9H-27KN8PB-71T
+# for example 1QQ3Y21KN-1DZH998-8RG
+# TODO: check that cmap name does not already exist in folder, if it does exist, ask user if want to overwrite
+# if they do, then make sure res-meta of new version contains <dc:source> tag 
+# otherwise rename to map with version#, or get name for new map from user
+
 folderID =  sys.argv[1]
 
 # Get the credentials
@@ -18,7 +22,8 @@ password = sys.argv[3]
 
 
 # Load cxl file
-f = open('templateMap.cxl')
+#f = open('test.cxl')
+f = open('Test Concept Map3.cxl')
 cxl = f.read()
 print 'Here is the cxl text: ' + cxl
 #
@@ -27,45 +32,15 @@ print 'Here is the cxl text: ' + cxl
 #data = f.read()
 
 # save cmap with res-meta as part of the cxl according to email titled "Fwd: User trying to use our API for CmapServer" on April 24, 2017
-# the Cmap and getting back the token ID which is used in the subsequent commands
+# the Cmap and getting back the last stanza of the <dc:source> token ID which is used in the subsequent commands
+# creation of new maps within folders should contain no <dc:source> 
+     # tag (map nodes will not show up in new maps if this exists),
+# saving over existing cmaps requires <dc:source> in res-meta file of replacement cmap 
+     # (map nodes will not show up if <dc:source> DOES NOT exist in in res-meta submission
 command = '/?cmd=save.cmap'
 start_url = '%s%s%s' % (baseURL, folderID, command)
 post_req = requests.post(start_url, auth=(username, password), allow_redirects=True, data=cxl)
 resourceID = post_req.text
+# howto crawl res-meta response to get just the <dc:source> tag which contains folder hierarchy in ':' separated stanzas
 print 'Here is the post response: ' + resourceID
 
-command = '/?cmd=write.resource.part'
-partname = '&partname=cmap'
-mimetype = '&mimetype=xml'
-url_suffix = command + partname + mimetype
-append_url = baseURL + folderID + url_suffix
-print 'Here is the append URL:  ' + append_url
-post_req = requests.post(append_url, auth=(username, password), allow_redirects=True, data=cxl)
-
-
-command = '/?cmd=done.saving.resource'
-url_suffix = command
-save_url = baseURL + folderID + url_suffix
-post_req = requests.post(save_url, auth=(username, password), allow_redirects=True)
-
-
-#Below code from cmapPost.py commented here bec. unnecessary
-## Start the process by telling the service the folder where we want to create
-## the Cmap and getting back the token ID which is used in the subsequent commands
-#command = '/?cmd=begin.creating.resource'
-#start_url = '%s%s%s' % (baseURL, folderID, command)
-#start_req = requests.post(start_url, auth=(username, password), allow_redirects=True, data=res_meta)
-#resourceID = start_req.text
-#print resourceID
-
-## Create the resource, passing the CXL file
-#command = '/?cmd=write.resource.part'
-#create_url = '%s%s%s&partname=cmap&mimetype=%s' % (baseURL, resourceID, command, mime_type)
-#create_req = requests.post(create_url, auth=(username, password), allow_redirects=True, data=data)
-#print create_req.text
-
-## Tell the server that we're done with the upload process for this Cmap
-#command = '/?cmd=done.saving.resource'
-#finish_url = '%s%s%s' % (baseURL, resourceID, command)
-#finish_req = requests.post(finish_url, auth=(username, password), allow_redirects=True)
-#print finish_req.text
